80ffbcf8a12ed27045e3ec5c8b6f683d6c233dc8
yewei0221 <763898518@qq.com>
2019-03-16 16:44:26 +0800

    yewei

--- src/js/ZZH_Goods_details.js
+++ src/js/ZZH_Goods_details.js
@@ -1,59 +1,47 @@
 $(function () {
-    zzhGoodsDetails();
-    zzhDetailsList();
+    init();
 
-    // 详情页ajax
-
-
-    // 面包屑导航
-    function zzhGoodsDetails() {
-        let productid = getUrl("productid")
-        $.get("http://193.112.55.79:9090/api/getproduct", { productid: productid }, (result) => {
-            let data = result.result;
-            // console.log(data)
-            let HTML = template("zzh_navtpl", { zhu: data });
-            $(".zzh_nav").html(HTML);
-
-        })
-
-        function getUrl(name) {
-            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-            var r = window.location.search.substr(1).match(reg);
-            if (r != null) return decodeURI(r[2]);
-            return null;
-        }
-
-
-
+    function init() {
+        getproduct();
+        getproductcom();
     }
 
-
-
-    // 详情页内容
-    function zzhDetailsList() {
-        let productid = getUrl("productid")
-        $.get("http://193.112.55.79:9090/api/getproduct", { productid: productid }, (result) => {
-            let data = result;
-            console.log(data)
-            // let HTML = template("zzh_navtpl", { zhu: data });
-            // $(".zzh_nav").html(HTML);
-
-        })
-
-        function getUrl(name) {
-            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-            var r = window.location.search.substr(1).match(reg);
-            if (r != null) return decodeURI(r[2]);
-            return null;
-        }
-
-
+    // 获取商品详情
+    function getproduct() {
+        $.ajax({
+            url: 'http://193.112.55.79:9090/api/getproduct',
+            type: 'get',
+            data: {
+                productid : getUrl('productid') || 1
+            },
+            success: (result) => {
+                let data = result.result;
+                let html = template('getProduct', {arr : data});
+                $('.bijia_goods').html(html);
+            }
+        });
     }
 
+    function getproductcom() {
+        $.ajax({
+            url: 'http://193.112.55.79:9090/api/getproductcom',
+            type: 'get',
+            data: {
+                productid : getUrl('productid') || 1
+            },
+            success: (result) => {
+                let data = result.result;
+                let html = template('getProductcom', {arr : data});
+                $('.comment_list').html(html);
+            }
+        });
+    }
 
-
-
-
-
-
+    // 获取 url 上数据
+    function getUrl(name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) return decodeURI(r[2]);
+        return null;
+    }
 })
