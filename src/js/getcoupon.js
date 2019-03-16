9d25f483373d084bb686062da38fc2aa0ca955f4
awsl <awsl@163.com>
2019-03-16 10:36:46 +0800

    优惠券轮播

--- src/js/getcoupon.js
+++ src/js/getcoupon.js
@@ -15,24 +15,49 @@ $(function(){
           dataType: "json",
           success: function (result) {
             let data=result.result;
-            console.log(data)
+            // console.log(data)
             let html=template('lessPrice',{arr:data});
             // console.log(html)
             $('.List_nav').html(html)
+            let html2 = template('slidertpl',{arr:data});
+            $('.swiper-wrapper').html(html2)
+            
+            
           }
       });
 
       //点击弹出遮罩层
-      $('.List_nav').on('tap',function(){
-        // $('#mark').css({display:'block'})
-        //  let id=$(this).attr()
-        //  console.log(id)
-        let height=$('body').height()
-         $('#mark').css({height:height})
+      $('.List_nav').on('tap','li',function(){
+          $(".mask").css("display","block")
+            // var gallery = mui('.mui-slider');
+            // gallery.slider({
+            // interval:500//自动轮播周期，若为0则不自动播放，默认为0；
+            // }); 
+            var mySwiper = new Swiper ('.swiper-container', {
+                direction: 'horizontal', // 垂直切换选项
+                loop: true, // 循环模式选项
+                
+                // 如果需要分页器
+                pagination: {
+                  el: '.swiper-pagination',
+                },
+                
+                // 如果需要前进后退按钮
+                navigation: {
+                  nextEl: '.swiper-button-next',
+                  prevEl: '.swiper-button-prev',
+                },
+                
+                // 如果需要滚动条
+                scrollbar: {
+                  el: '.swiper-scrollbar',
+                },
+              })        
       })
 
+      
     }
-
+    
 
 
         //获取url参数
